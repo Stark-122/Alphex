10bac25726d70d15edc3058be3e1203c5d1153f9
Hari Krishna <starkanthony122@gmail.com>
2023-03-05 08:05:59 +0530

    Compleed Tokenization, and data split (closes #2)

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
-
+import torch 
+#Reading Data from given DataSet
 with open('DataSet/tiny-shakespeare.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
@@ -10,3 +11,20 @@ vocab_size = len(chars)
 print(''.join(chars))
 print(vocab_size)
 
+#Tokenization
+stoi = { ch:i for i , ch in enumerate(chars) }
+itos = { i:ch for i , ch in enumerate(chars) }
+
+encode = lambda s: [stoi[c] for c in s] #Encoder : Takes string input, gives integers and output
+decode = lambda l: ''.join([itos[i] for i in l]) #Decoder : Takes integer input, and returns string output
+
+print(encode("Hello there"))
+print(decode([20, 43, 50, 50, 53, 1, 58, 46, 43, 56, 43]))
+
+data = torch.tensor(encode(text), dtype=torch.long)
+print(data.shape, data.dtype)
+print(data[:1000])
+
+n= int(0.9 * len(data)) #Takes 90% of data
+train_data = data[:n] #Uses the 90% of Data as Training Value
+val_data = data[n:] #Rest 10% of data as valuation Data
