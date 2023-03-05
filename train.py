079376c312d3da8956fc007d07cbbefe9c9ee0a1
Hari Krishna <starkanthony122@gmail.com>
2023-03-05 07:10:01 +0530

    Added the ability to read Data (closes #1)

--- train.py
+++ train.py
@@ -5,3 +5,8 @@ with open('DataSet/tiny-shakespeare.txt', 'r', encoding='utf-8') as f:
 print(f"Length of dataset in characters: {len(text)}")
 print(text[:1000])
 
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+print(''.join(chars))
+print(vocab_size)
+
